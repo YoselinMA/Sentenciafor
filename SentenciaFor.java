a839aa49696dc396baab6fabef7d7eee36a1fca5
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-04-19 22:57:55 -0600

    Create SentenciaFor.java

---
+++ SentenciaFor.java
@@ -0,0 +1,18 @@
+public class SentenciaFor {
+    public static void main(String[] args) {
+        
+    
+
+        int longitudSerie = 50;
+        int i;
+        for(i = 1;  i<=(longitudSerie/2); i++){
+            int tot = 2 * i;
+            int total =  3 *i;
+            System.out.println("El resultado es: "+ tot);
+            
+            System.out.println("El resultado es: "+ total);
+        }
+    }
+
+    
+}
